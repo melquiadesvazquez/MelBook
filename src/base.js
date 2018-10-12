356f2f51b207c6c9a0a93bda2246f6e825cfd201
Melquiades Vazquez <melquiades.vazquez@gmail.com>
2018-10-12 11:40:41 +0200

    updated env variables

--- src/base.js
+++ src/base.js
@@ -1,10 +1,10 @@
 import Rebase from "re-base";
-import firebase from "firebase"
+import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.DB_API_KEY,
-  authDomain: process.env.DB_AUTH_DOMAIN,
-  databaseURL: process.env.DB_URL
+  apiKey: process.env.REACT_APP_DB_API_KEY,
+  authDomain: process.env.REACT_APP_DB_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DB_URL
 });
 
 const base = Rebase.createClass(firebaseApp.database());
